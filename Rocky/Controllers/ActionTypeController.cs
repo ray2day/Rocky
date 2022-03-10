560da485e407133f68321aee119a6c865e1d5bae
Raymond van Hoorn <vanhoornraymond@gmail.com>
2022-03-10 15:47:07 +0100

    Added Category and made assignment which was Adding Application Type

--- Rocky/Controllers/ActionTypeController.cs
+++ Rocky/Controllers/ActionTypeController.cs
@@ -8,17 +8,17 @@ using System.Threading.Tasks;
 
 namespace Rocky.Controllers
 {
-    public class CategoryController : Controller
+    public class ApplicationTypeController : Controller
     {
         private readonly ApplicationDbContext _db;
 
-        public CategoryController(ApplicationDbContext db)
+        public ApplicationTypeController(ApplicationDbContext db)
         {
             _db = db;
         }
         public IActionResult Index()
         {
-            IEnumerable<Category> objList = _db.Category;
+            IEnumerable<ApplicationType> objList = _db.ApplicationType;
             return View(objList);
         }
 
@@ -31,9 +31,9 @@ namespace Rocky.Controllers
         // POST - CREATE
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Category obj)
+        public IActionResult Create(ApplicationType obj)
         {
-            _db.Category.Add(obj);
+            _db.ApplicationType.Add(obj);
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
